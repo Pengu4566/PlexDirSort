25a024310256a3ce0396d12e97147b4051918ff0
MKonos <Mihalis4566@gmail.com>
2020-02-12 22:20:19 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+#first commit, nothing here!
+
+import sys
+import os
+
+
+
+def movie_unzip():
+    print(1)
+
+def movie_sort():
+    print(1)
+
+def main():
+
+    print(str(sys.argv))
+    #os.mkdir(str("C:/Users/mihal/Desktop/" + sys.argv[1]))
+
+    print("I did it")
+
+
+
+
+main()
