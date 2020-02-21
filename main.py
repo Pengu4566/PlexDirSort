493db5f4ccbb7dfcf2b19c8f016dc1084935b6c1
MKonos <Mihalis4566@gmail.com>
2020-02-20 22:50:26 -0600

    program now unzips game drives to Z for further processing. also checks for mp3 file percentages initially alongside name checking, either can pass for music verification. program also checks for existing files when copying

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import re
 import sys
 import glob
 import shutil
+import os.path
 import subprocess
 import qbittorrentapi
 from shutil import copy
@@ -13,10 +14,12 @@ from datetime import datetime
 import PathGenerator as PathGen
 
 ### SETTINGS ###
+GAME_DESTINATION = "Z:\\"
 MOVIE_TV_DESTINATION = "Z:\\"
 MUSIC_DESTINATION = "C:\\Users\\mihal\\Desktop"
 LOG_DESTINATION = "C:\\Users\\mihal\\Desktop"
 TORRENT_FOLDER = "H:\\Torrent\\Downloaded Torrents"
+MUSIC_CONFIDENCE_REQUIRED_SCORE = 0.65
 ### SETTINGS ###
 
 # uses QBitTorrent api to control the current torrent
@@ -33,14 +36,16 @@ def logToFile(log_text):
 def torrentNameCheck_Movie(torrent_name):
     if torrent_name.__contains__("x264") | torrent_name.__contains__("1080p") | torrent_name.__contains__("720p") | torrent_name.__contains__("XVID"):
         return True
+
 def torrentNameCheck_Music(torrent_name):
     if torrent_name.__contains__("FLAC") | torrent_name.__contains__("320") | torrent_name.__contains__("v0") | torrent_name.__contains__("v2") | torrent_name.__contains__("WAV"):
         return True
 
-#checks torrent file extension (from path)
+# checks torrent file extension (from path)
 def fileTypeCheck_Movie(torrent_root_path):
     if str(torrent_root_path).__contains__(".mkv") | str(torrent_root_path).__contains__(".avi") | str(torrent_root_path).__contains__(".mp4") | str(torrent_root_path).__contains__(".wmv") and not str(torrent_root_path).__contains__("sample") | str(torrent_root_path).__contains__("Sample") | str(torrent_root_path).__contains__("SAMPLE"):
         return True
+
 def fileTypeCheck_TV(torrent_root_path):
     if str(torrent_root_path).__contains__(".mkv") | str(torrent_root_path).__contains__(".avi") | str(torrent_root_path).__contains__(".mp4") | str(torrent_root_path).__contains__(".wmv"):
         return True
@@ -49,6 +54,35 @@ def fileTypeCheck_TV(torrent_root_path):
 def fileTypeCheck_Music(torrent_file):
     if str(torrent_file).__contains__(".mp3") | str(torrent_file).__contains__(".flac") | str(torrent_file).__contains__(".FLAC") | str(torrent_file).__contains__(".wav") | str(torrent_file).__contains__(".WAV"):
         return True
+
+def fileTypeCheck_Game(torrent_root_path):
+    if str(torrent_root_path).__contains__(".iso") | str(torrent_root_path).__contains__(".rar") | str(torrent_root_path).__contains__(".img") | str(torrent_root_path).__contains__("CRACK") | str(torrent_root_path).__contains__("crack"):
+        return True
+
+def fileTypeCheck_Game_Zipped(torrent_root_path):
+    if str(torrent_root_path).__contains__(".rar"):
+        return True
+
+def musicIdentifier(torrent_root_path):
+    confirm_number = 0
+    for (root,dirs,files) in os.walk(torrent_root_path):
+        for file in files:
+            if fileTypeCheck_Music(file):
+                confirm_number += 1
+                if confirm_number / len(files) > MUSIC_CONFIDENCE_REQUIRED_SCORE:
+                    return True
+
+def gameIdentifier(torrent_root_path):
+    # confirm_number(0, not a game | 1, game, normal | 2, game, zipped)
+    confirm_number = 0
+    for (root, dirs, files) in os.walk(torrent_root_path):
+        for file in files:
+            if fileTypeCheck_Game(file):
+                confirm_number = 1
+                if fileTypeCheck_Game_Zipped(file):
+                    return 2
+
+    return confirm_number
 
 # thanks to renanbs on GitHub: https://github.com/renanbs/extractor/blob/master/LICENSE
 def multiRarUnzip(torrent_name, torrent_root_path):
@@ -65,44 +99,22 @@ def multiRarUnzip(torrent_name, torrent_root_path):
 
 def copySomething(root_folder, file_to_copy, path_to_copy_to):
     logToFile(str(datetime.now()) + " [copySomething] - copying file: " + file_to_copy + " to destination: " + path_to_copy_to + "\n")
-    copy(str(root_folder + "\\" + file_to_copy), path_to_copy_to)
-    logToFile(str(datetime.now()) + " [copySomething] - COPY COMPLETE \n")
+    print(os.path.exists(path_to_copy_to))
+    if not os.path.exists(path_to_copy_to):
+        copy(str(root_folder + "\\" + file_to_copy), path_to_copy_to)
+        logToFile(str(datetime.now()) + " [copySomething] - COPY COMPLETE \n")
 
-def musicSort(torrent_name, torrent_root_path):
-    confirm_number = 0
-    confirmation = False
-
-    for (root,dirs,files) in os.walk(torrent_root_path):
-        for file in files:
-            # copies all mkv files in torrent folder
-            if fileTypeCheck_Music(file):
-                # copy the whole folder, not just individual files in folder
-                print(file)
-                confirm_number += 1
-                no_association = False
-
-                # if more than 65% of files in folder are music, this is most likely an album
-                if confirm_number / len(files) > 0.65:
-                    confirmation = True
-
-    # files inside torrent are verified to be music files
-    if confirmation == True:
-        shutil.copytree(torrent_root_path, MUSIC_DESTINATION + "/" + torrent_name)
-
-# TODO handle torrents that just leave a file in the downloads folder
-
-    # check to see if the above logic didnt capture a file
-    if no_association == True:
-        logToFile("[musicSort] - NO ASSOCIATIONS FOR -MUSIC- TORRENT - " + torrent_name + " \n")
+    else:
+        logToFile(str(datetime.now()) + " [copySomething] - FILE ALREADY EXISTS \n")
 
 def tvSort(torrent_name, torrent_root_path):
-    for (root,dirs,files) in os.walk(torrent_root_path):
+    no_association = True
+    for (root, dirs, files) in os.walk(torrent_root_path):
         for file in files:
             # copies all mkv files in torrent folder
             if fileTypeCheck_TV(file):
                 copySomething(root, file)
                 logToFile(file + "***\n")
-
                 no_association = False
 
             # rar file discovered, begin unzipping
@@ -111,12 +123,11 @@ def tvSort(torrent_name, torrent_root_path):
                 no_association = False
 
     # check to see if the above logic didnt capture a file
-    if no_association == True:
+    if no_association:
         logToFile("[tvSort] - NO ASSOCIATIONS FOR -TV- TORRENT - " + torrent_name + " \n")
 
 def movieSort(torrent_name, torrent_root_path):
     no_association = True
-
     for (root,dirs,files) in os.walk(torrent_root_path):
         for file in files:
             # copies all mkv files in torrent folder except small SAMPLE clips
@@ -134,13 +145,28 @@ def movieSort(torrent_name, torrent_root_path):
     if no_association == True:
         logToFile("[movieSort] - NO ASSOCIATIONS FOR -MOVIE- TORRENT - " + torrent_name + " \n")
 
+# music has been verified by this point, just perform the copy
+def musicSort(torrent_name, torrent_root_path):
+    path_to_copy_to = MUSIC_DESTINATION + "/" + torrent_name
+    if not os.path.exists(path_to_copy_to):
+        logToFile(str(datetime.now()) + " [musicSort] - copying file: " + torrent_name + " to destination: " + MUSIC_DESTINATION + "\n")
+        shutil.copytree(torrent_root_path, path_to_copy_to)
+        logToFile(str(datetime.now()) + " [musicSort] - COPY COMPLETE \n")
+    else:
+        logToFile(str(datetime.now()) + " [musicSort] - FILE ALREADY EXISTS \n")
+
+# game has been verified by this point
+def gameSort(torrent_name, torrent_root_path):
+    path_to_copy_to = GAME_DESTINATION + "/" + torrent_name
+    if not os.path.exists(path_to_copy_to):
+        logToFile(str(datetime.now()) + " [gameSort] - copying file: " + torrent_name + " to destination: " + GAME_DESTINATION + "\n")
+        shutil.copytree(torrent_root_path, path_to_copy_to)
+        logToFile(str(datetime.now()) + " [gameSort] - COPY COMPLETE \n")
+
+    else:
+        logToFile(str(datetime.now()) + " [gameSort] - FILE ALREADY EXISTS \n")
+
 def main():
-
-    # TODO make games copy/unzip to z drive
-    # TODO check for existing files during copy
-    # TODO put mp3 check before torrent name check for music only
-    # TODO upgrade music percentage checker to drill recursively through folders
-
     print(str(sys.argv))
     torrent_name = sys.argv[1]
     torrent_root_path = sys.argv[2]
@@ -178,10 +204,13 @@ def main():
         else:
             movieSort(torrent_name, torrent_root_path)
 
-    # might be music too
-    elif torrentNameCheck_Music(torrent_name):
+    # might be music too, must check torrent name and have more than 65% music files inside
+    elif torrentNameCheck_Music(torrent_name) or musicIdentifier(torrent_root_path):
         logToFile(str(datetime.now()) + " [main] - MUSIC detected \n")
         musicSort(torrent_name, torrent_root_path)
+
+    elif gameIdentifier(torrent_root_path):
+        gameSort(torrent_name, torrent_root_path)
 
     else:
         logToFile("***NO ASSOCIATIONS FOR TORRENT - " + torrent_name + "***\n")
